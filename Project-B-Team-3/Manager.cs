51095583f475b7bf9f5a0027e77ab706ab11794a
Bryan Kartoikromo <1028655@hr.nl>
2022-03-28 10:58:55 +0200

    qaz

--- Project-B-Team-3/Manager.cs
+++ Project-B-Team-3/Manager.cs
@@ -12,7 +12,6 @@ namespace ProjectB
     internal class Manager
     {
         public int MainMenu_LastIndex = 0;
-        //test
         public void Start()
         {
             RunStartingMenu();
