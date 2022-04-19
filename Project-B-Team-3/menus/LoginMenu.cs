4a073062838887c64d487a20895584f675674d07
Roan van Velden <1024885@hr.nl>
2022-04-19 13:44:20 +0200

    Login Menu

--- Project-B-Team-3/menus/LoginMenu.cs
+++ Project-B-Team-3/menus/LoginMenu.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ProjectB
+{
+    internal class LoginMenu
+    {
+        public int Run()
+        {
+            Console.Clear();
+            ConsoleKeyInfo key;
+            api.PrintCenter("Login", 10);
+            do
+            {
+                key = Console.ReadKey(true);
+            }
+            while (key.Key != ConsoleKey.Escape);
+            return 0;
+        }
+    }
+}
