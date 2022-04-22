58655314d3a9b5a1bde112b4df75d2cf63d78a30
Nino Reichardt <nino@Nino>
2022-04-22 13:41:40 -0400

    s

--- Project-B-Team-3/classes/AccountJson.cs
+++ Project-B-Team-3/classes/AccountJson.cs
@@ -73,12 +73,7 @@ namespace ProjectB
             {
                 if (account.Email.Equals(email))
                 {
-<<<<<<< HEAD
-                    Console.WriteLine(account.Password);
-                    if (account.Password == password)
-=======
                     if (account.Password.Equals(password))
->>>>>>> c73dcc552319db7ed2dd7e23d8ee8ecc78f0e075
                     {
                         return account;
                     }
