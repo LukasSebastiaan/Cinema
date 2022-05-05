e057c86d78d31b40bcb62b6f7ac3e8de92c8c328
LukasSebastiaan <wevers.luke@gmail.com>
2022-05-05 20:49:34 +0200

    Review Json's

---
+++ Project-B-Team-3/classes/ReviewJson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ProjectB
+{
+    public class Review
+    {
+        [JsonPropertyName("Name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("Text")]
+        public string Text { get; set; }
+    }
+    internal class ReviewsList
+    {
+        public List<Review> Reviews;
+        private string ReviewJsonName = @$"Data{Path.DirectorySeparatorChar}Reviews.json";
+
+        public void Save()
+        {
+            var options = new JsonSerializerOptions();
+            options.WriteIndented = true;
+            File.WriteAllText(ReviewJsonName, JsonSerializer.Serialize(Reviews, options: options));
+        }
+
+        public void Load()
+        {
+            string json = File.ReadAllText(ReviewJsonName);
+            Reviews = JsonSerializer.Deserialize<List<Review>>(json);
+        }
+    }
+}
