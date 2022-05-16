84dfd86fe27e97fdec06ccf0997e01b53edfaec8
LukasSebastiaan <wevers.luke@gmail.com>
2022-05-16 13:34:08 +0200

    Reservation email

--- Project-B-Team-3/menus/ConfirmReservation.cs
+++ Project-B-Team-3/menus/ConfirmReservation.cs
@@ -16,7 +16,7 @@ namespace ProjectB
         public void FirstRender()
         {
             api.PrintCenter(Program.information.Member.Firstname + " " + Program.information.Member.Lastname + ", thank you for your reservation!", 13);
-            api.PrintCenter("An comfirmation email has been sent.", 14);
+            api.PrintCenter("An confirmation email has been sent.", 14);
             api.PrintCenter("Enjoy!", 15);
             api.PrintCenter("press Enter to confirm...", 17);
         }
@@ -34,7 +34,16 @@ namespace ProjectB
 
                 if (keyPressed == ConsoleKey.Enter)
                 {
-                    var Dict = new Dictionary<string, string>() { { "{{Username}}", info.Member.Email }, { "{{MovieDate}}", info.ChosenDate }, { "{{MovieTime}}", info.ChosenTime }, { "{{MovieTitle}}", info.ChosenFilm.Name }, { "{{Seats}}", info.ChosenSeats.ToString() }, { "{{PopcornAmount}}", info.PopcornAmount.ToString() } }; // Defined vars to be replaced in mail template
+                    int[][] seats = info.ChosenSeats;
+                    string SeatsStr = $"R{seats.First()[0]}S{seats.First()[1]}";
+                    foreach (int[] seat in seats)
+                    {
+                        if (seat != seats.First())
+                        {
+                            SeatsStr += $"|R{seat[0]}S{seat[1]}";
+                        }
+                    }
+                    var Dict = new Dictionary<string, string>() { { "{{Username}}", info.Member.Email }, { "{{MovieDate}}", info.ChosenDate }, { "{{MovieTime}}", info.ChosenTime }, { "{{MovieTitle}}", info.ChosenFilm.Name }, { "{{Seats}}", SeatsStr }, { "{{PopcornAmount}}", info.PopcornAmount.ToString() } }; // Defined vars to be replaced in mail template
                     SendEmail.SendVerifyEmail(Program.information.Member.Email, "htmlReservation.txt", Dict); // Sends the email
 
                     return 0;
